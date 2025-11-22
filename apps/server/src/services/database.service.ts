d2144dba0bc012e8ec1cc88f4124369e00d48e2b
CatOfJupit3r <catofjupit3r@gmail.com>
2025-11-22 17:51:28 +0200

    fix: do not connect to mongo in tests

--- apps/server/src/services/database.service.ts
+++ apps/server/src/services/database.service.ts
@@ -4,6 +4,7 @@ import env from '@~/constants/env';
 
 class DatabaseService {
   public async connect() {
+    if (process.env.NODE_ENV === 'test') return;
     await this.connectToExternalDatabase();
   }
 
